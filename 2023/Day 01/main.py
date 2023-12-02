a3a5ea9d94d4b5bfc561f0be5773bc7a2ffca1f8
Dawid Dieu <dawid.dieu@kalepa.com>
2023-12-02 12:04:26 +0100

    Day 1

--- 2023/Day 01/main.py
+++ 2023/Day 01/main.py
@@ -10,35 +10,30 @@ digits = [
     "eight",
     "nine",
 ]
+
+
+def is_number(x: str, i: int) -> str:
+    for d in digits:
+        if x[i: i + len(d)] == d:
+            return str(digits.index(d) + 1)
+
+        if x[i].isdigit():
+            return x[i]
+
+def solve2(x: str) -> int:
+    d1 = [is_number(x, i) for i in range(len(x))]
+    d1 = [x for x in d1 if x is not None][0]
+    d2 = [is_number(x, i) for i in reversed(range(len(x)))]
+    d2 = [x for x in d2 if x is not None][0]
+    return int(d1 + d2)
+
+
 def solve(x: str) -> int:
     nums = [it for it in x if it.isdigit()]
     if len(nums) == 1:
         return int(nums[0] + nums[0])
     return int(nums[0] + nums[-1])
 
-
-def get_digit(x: str) -> str:
-    for i in range(len(x)):
-        for d in digits:
-            if x[i: i + len(d)] == d:
-                return str(digits.index(d) + 1)
-
-            if x[i].isdigit():
-                return x[i]
-
-def get_digit_rev(x: str) -> str:
-    for i in reversed(range(len(x))):
-        for d in digits:
-            if x[i: i + len(d)] == d:
-                return str(digits.index(d) + 1)
-
-            if x[i].isdigit():
-                return x[i]
-
-def solve2(x: str) -> int:
-    d1 = get_digit(x)
-    d2 = get_digit_rev(x)
-    return int(d1 + d2)
 
 def main():
     with open("data.txt") as f:
